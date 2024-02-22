f4dad63eb33c536210aa0c057e5cff91e4412650
HakimHC <ahmedh90111@gmail.com>
2024-02-22 10:39:11 +0100

    Added a simple logger

---
+++ src/logger.py
@@ -0,0 +1,24 @@
+import json
+import logging
+import logging.config
+
+
+class Logger:
+    def __init__(self, config):
+        self.config = config
+        self.setup_logging()
+
+    def setup_logging(self):
+        logging_config = self.config.get('logging', {})
+        log_file = logging_config.get('log_file', 'deployment.log')
+        log_level = logging_config.get('log_level', 'INFO').upper()
+        log_format = logging_config.get('log_format', '[%(asctime)s] - %(levelname)s - %(message)s')
+
+        logging.basicConfig(filename=log_file, level=log_level, format=log_format, filemode='a')
+
+        console_handler = logging.StreamHandler()
+        console_handler.setLevel(log_level)
+        formatter = logging.Formatter(log_format)
+        console_handler.setFormatter(formatter)
+        logging.getLogger('').addHandler(console_handler)
+
