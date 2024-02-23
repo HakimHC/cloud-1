1fce750c8789cc868b353d4d8e3ae09f4426569b
HakimHC <ahmedh90111@gmail.com>
2024-02-23 14:59:54 +0100

    Refactored executor class

--- src/executor.py
+++ src/executor.py
@@ -8,13 +8,32 @@ from constants import PROJECT_ROOT
 
 class CommandExecutor:
     @staticmethod
-    def execute_commands(commands: list[str], working_directory=None, capture=False):
-        output = []
+    def execute_commands(commands: list[str], working_directory=None, capture=False, retries=1):
         logging.debug(f'Executing commands: {commands}')
 
         if working_directory:
             os.chdir(working_directory)
 
+        i = 0
+        ret = {}
+        while i < retries:
+            ret = CommandExecutor.__exec(commands, capture)
+            if ret.get('exit_code') == 0:
+                break
+            i += 1
+
+        if i == retries:
+            logging.error(f'Command execution failed. ({commands})')
+            exit(1)
+        os.chdir(PROJECT_ROOT)
+        return [ret.get('stdout')]
+
+    @staticmethod
+    def __exec(
+            commands: list[str],
+            capture: bool
+    ):
+        ret = {}
         for command in commands:
             if capture:
                 result = subprocess.run(
@@ -30,12 +49,10 @@ class CommandExecutor:
                     stdout=sys.stdout,
                     stderr=sys.stderr
                 )
+            ret["stdout"] = result.stdout
+            ret["stderr"] = result.stderr
+            ret["exit_code"] = result.returncode
 
-            output.append(result.stdout)
             if result.returncode != 0:
-                logging.error(result.stderr)
-                exit(1)
-
-        os.chdir(PROJECT_ROOT)
-        return output
-
+                return ret
+        return ret
