d9c135121e4b2978c7cf9161975213b848d1da63
HakimHC <ahmedh90111@gmail.com>
2024-02-23 15:00:12 +0100

    Added default logfile.

--- src/logger.py
+++ src/logger.py
@@ -14,11 +14,21 @@ class Logger:
         log_level = logging_config.get('level', 'INFO').upper()
         log_format = logging_config.get('format', '[%(asctime)s] - %(levelname)s - %(message)s')
 
-        logging.basicConfig(filename=log_file, level=log_level, format=log_format, filemode='a')
+        warn = False
+
+        try:
+            logging.basicConfig(filename=log_file, level=log_level, format=log_format, filemode='a')
+        except (FileNotFoundError, PermissionError):
+            log_file = './deployment.log'
+            logging.basicConfig(filename=log_file, level=log_level, format=log_format, filemode='a')
+            warn = True
 
         console_handler = logging.StreamHandler()
         console_handler.setLevel(log_level)
         formatter = logging.Formatter(log_format)
         console_handler.setFormatter(formatter)
         logging.getLogger('').addHandler(console_handler)
+
+        if warn:
+            logging.warning('Logging was not possible in the configurated logfile, logs are saved to: ./deployment.log')
 
